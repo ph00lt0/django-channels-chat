3f99516efb555cfed350aac85e981274f9073661
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-04-22 21:40:53 +0200

    changed import

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,8 +1,8 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
